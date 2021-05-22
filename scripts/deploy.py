890bd25a65185f4c5b41398c680f670ffa2f33d2
Mike Burr <mburr@unintuitive.org>
2021-05-22 17:04:09 -0400

    Just pretened this is about ERC721 for now. Playing with Vyper and testing...

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,11 +2,15 @@
 
 import os
 import json
-from brownie import t3st, accounts, config
+from brownie import Test721, accounts, config
 from dotenv import load_dotenv
 load_dotenv()
 
+# starting with a vyper erc721 contract to understand testing.
 def main():
+    token_holder_number = 5  # It's nice to use the same number for a few things
+    token_holder = accounts[token_holder_number]
     dev = accounts.add(config["wallets"]["from_key"])
-    contract = t3st.deploy({"from": dev}, publish_source=False)
-    print(contract)
+    contract = Test721.deploy({"from": dev}, publish_source=False)
+    ret = contract.mint(token_holder, token_holder_number)
+    assert contract.ownerOf(token_holder_number) == token_holder.address
